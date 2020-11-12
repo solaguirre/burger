d0117747200f0994469e5c651e11744dd24a3165
solaguirre <68126593+solaguirre@users.noreply.github.com>
2020-11-11 23:49:33 -0700

    Addded link to css stylesheet.

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ USE burgers_db;
 
 CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(30),
+    burger_name VARCHAR(255),
     devoured BOOLEAN DEFAULT FALSE,
     PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,2 @@
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("Veggie", true), ("Salmon", false), ("Cheeseburger", true)
+INSERT INTO burgers (burger_name, devoured, createdAt)
+VALUES ("Veggie", true, "2020-11-12-06-31-39"), ("Salmon", false, "2020-11-12-07-31-39"), ("Cheeseburger", true, "2020-11-12-08-31-39")
