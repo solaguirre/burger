66b4f719d979def25c739ac9e164fd09684ab5ec
solaguirre <68126593+solaguirre@users.noreply.github.com>
2020-11-11 21:39:04 -0700

    Updated orm.js file with methods to execute Mysql.

--- config/orm.js
+++ config/orm.js
@@ -1,32 +1,65 @@
 const connection = require("./connection.js");
 
-function objToSql(object) {
-    const array = [];
+// function objToSql(object) {
+//     const array = [];
 
-    for (var key in object) {
-        // push the key and the value into the array 
+//     for (var key in object) {
+//         // push the key and the value into the array 
 
-        array.push(key + "=" + object[key])
-    }
+//         array.push(key + "=" + object[key])
+//     }
 
-    return array.toString();
-}
+//     return array.toString();
+// }
 
 // will communicate with
 const orm = {
 
-    selectAll() {
+    selectAll: function (tableInput, cb){
+        const queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result){
+            if (err){
+                throw err;
+            }
+            cb(result);
+        })
+    },
+
+    insertOne: function (table,columns, values,cb) {
+        let queryString = "INSERT INTO " + table;
+
+        queryString += "(";
+        queryString += columns.toString();
+        queryString += ")";
+        queryString += " VALUES (";
+        queryString += "?";
+        queryString += ")";
+
+        connection.query(queryString,values, function (err, result){
+            if (err) throw err;
+
+            cb(result);
+         });
 
     },
 
-    insertOne() {
+    updateOne: function(table, objColVals, condition, cb) {
+        let queryString = "UPDATE" + table;
+
+        queryString += " SET ";
+        queryString += objColVals;
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, function(err, result){
+            if (err) throw err;
+
+            cb(result);
+
+        });
 
     },
 
-    updateOne() {
-
-    }
-
-}
+};
 
 module.exports = orm;
