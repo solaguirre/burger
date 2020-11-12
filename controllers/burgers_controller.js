5181b11d32716ae3a112e4923792db7c9c243554
solaguirre <68126593+solaguirre@users.noreply.github.com>
2020-11-12 00:32:57 -0700

    Updated controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,29 +6,30 @@ const burger = require("../models/burger.js");
 
 // routes
 
-router.get("/",function(req, res){
-    burger.selectAll(function(data){
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
         const hbsObject = {
             burgers: data
         };
+        res.render("index",hbsObject);
     });
 });
 
-router.post("/api/burgers",function(req,res){
-    burger.insertOne([req.body.name], function(result){
-        res.json({id: result.insertId});
+router.post("/api/burgers", function (req, res) {
+    burger.insertOne([req.body.name], function (result) {
+        res.json({ id: result.insertId });
     });
 });
 
-router.put("/api/burgers/:id", function(req,res){
-    const condition ="id="+ req.params.id;
+router.put("/api/burgers/:id", function (req, res) {
+    const condition = "id=" + req.params.id;
 
-    console.log("condition",condition);
+    console.log("condition", condition);
 
-    burger.updateOne(condition, function (result){
+    burger.updateOne(condition, function (result) {
         if (result.changedRows == 0) {
             return res.status(404).end();
-        }else {
+        } else {
             res.status(200).end();
         }
     });
